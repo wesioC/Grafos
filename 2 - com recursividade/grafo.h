d721941d3bd45227b12fc97ec2a8dd2f13731dd2
Wesio Coelho <102686677+wesioC@users.noreply.github.com>
2022-05-25 18:10:00 -0300

    Delete 2 - com recursividade directory

--- 2 - com recursividade/grafo.h
+++
@@ -1,8 +0,0 @@
-#define vertex int
-typedef struct graph *Graph;
-typedef struct node *link;
-Graph GRAPHinit(int V);
-void GRAPHinsertArc(Graph G, vertex v, vertex w);
-void GRAPHremoveArc(Graph G, vertex v, vertex w);
-void GRAPHshow(Graph G);
-void GRAPHleitura(Graph G, int m);
