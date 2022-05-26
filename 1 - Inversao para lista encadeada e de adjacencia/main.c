eb42b239d2c3fd8a23bc2e6ef3bc191cbcab5e69
Wesio Coelho <102686677+wesioC@users.noreply.github.com>
2022-05-25 21:29:00 -0300

    Add files via upload

--- 1 - Inversao para lista encadeada e de adjacencia/main.c
+++ 1 - Inversao para lista encadeada e de adjacencia/main.c
@@ -1,20 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "grafo.h"
+struct graph
+{
+   int V;
+   int A;
+   int **adj;
+};
+static int **MATRIXint(int r, int c, int val)
+{
+   vertex i, j;
+   int **m = malloc(r * sizeof(int *));
+   for (i = 0; i < r; ++i)
+      m[i] = malloc(c * sizeof(int));
+   for (i = 0; i < r; ++i)
+      for (j = 0; j < c; ++j)
+         m[i][j] = val;
+   return m;
+}
+void GRAPHleitura(Graph G, int m)
+{
+   int a, b;
+   while (m--)
+   {
+      scanf("%d %d", &a, &b);
+      GRAPHinsertArc(G, a, b);
+      GRAPHinsertArc(G, b, a);
+   }
+}
+Graph GRAPHinit(int V)
+{
+   Graph G = malloc(sizeof *G);
+   G->V = V;
+   G->A = 0;
+   G->adj = MATRIXint(V, V, 0);
+   return G;
+}
+
+void GRAPHinsertArc(Graph G, vertex v, vertex w)
+{
+
+   if (G == NULL)
+      return;
+   if (v < 0 || v >= G->V)
+      return;
+   if (w < 0 || w >= G->V)
+      return;
+
+   if (G->adj[v][w] == 0)
+   {
+      G->adj[v][w] = 1;
+      G->A++;
+   }
+}
+
+void GRAPHremoveArc(Graph G, vertex v, vertex w)
+{
+   if (G == NULL)
+      return;
+   if (v < 0 || v >= G->V)
+      return;
+   if (w < 0 || w >= G->V)
+      return;
+
+   if (G->adj[v][w] != 0)
+   {
+      G->adj[v][w] = 0;
+      G->A--;
+   }
+}
+
+void GRAPHshow(Graph G)
+{
+   vertex v, w;
+   for (v = 0; v < G->V; v++)
+   {
+      printf("%d:", v);
+      for (w = 0; w < G->V; ++w)
+      {
+         printf(" %d", G->adj[v][w]);
+      }
+      printf("\n");
+   }
+   printf("Vértices: %d Arestas: %d\n", G->V, G->A);
+}
+
+
 //----------------------Matriz adjacencia para incidencia-------------------------
-void Matrizadj_Inc(int n){
-  int ma[n][n];
+void Matrizadj_Inc(int n, Graph ma){
   int qtdvertices = 0;
   int cont = 0;
 //-----------------------------------------------------
   //ler matriz de adjacencia
+  /*
   printf("Escreva a matriz de adjacência:\n");
   for(int i = 0; i<n;i++){
     for(int j = 0; j<n;j++){
       scanf("%i", &ma[i][j]);
   }
   }
-  /*
+  
   printf("\n matriz de adjacência:");
   printf("\n  ");
   for(int j = 0; j<n;j++){
@@ -29,28 +115,28 @@ void Matrizadj_Inc(int n){
   */
 //-----------------------------------------------------
 //verificar quantidade de vertices
-  for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
+  for (int i = 0; i < ma->V; i++)
+        for (int j = 0; j < ma->V; j++) {
             if (i <= j) {
-                qtdvertices += ma[i][j];
+                qtdvertices += ma->adj[i][j];
             }
         }
   printf("\nEsse grafo tem %i arestas\n\n",qtdvertices);
 //-----------------------------------------------------
   //zerar matriz
-  int mi[n][qtdvertices];
+  int mi[ma->V][qtdvertices];
   for(int i = 0; i<n;i++)
     for(int j = 0; j<qtdvertices;j++)
       mi[i][j]=0;
 //-----------------------------------------------------
    //tranformar em matriz de incidencia
-  for(int i = 0; i<n;i++){
-    for(int j = 0; j<n;j++){
-      if(i == j && ma[i][j]){
+  for(int i = 0; i<ma->V;i++){
+    for(int j = 0; j<ma->V;j++){
+      if(i == j && ma->adj[i][j]==1){
         mi[i][cont]=2;
         cont++;
       }
-      if(i<j && ma[i][j]==1){
+      if(i<j && ma->adj[i][j]==1){
         mi[i][cont]=1;
         mi[j][cont]=1;
         cont++;
@@ -62,7 +148,7 @@ void Matrizadj_Inc(int n){
   for(int j = 0; j<qtdvertices;j++){
     printf(" %i",j);
   }
-  for(int i = 0; i<n;i++){
+  for(int i = 0; i<ma->V;i++){
     printf("\n%i: ",i);
     for(int j = 0; j<qtdvertices;j++){
       printf("%i ", mi[i][j]);
@@ -237,17 +323,10 @@ void listapMatriz(Lista *lista,int m){
   
 }
 //-----------------------------------------------------
-void matrizpLista(int m,Lista *lista){
-  int ma[m][m];
-    printf("Escreva a matriz de adjacência:\n");
-    for(int i = 0; i<m;i++){
-      for(int j = 0; j<m;j++){
-        scanf("%i", &ma[i][j]);
-      }
-    }
-     for(int i = 0; i<m;i++){
-      for(int j = 0; j<m;j++){
-        inserir_fim(lista,ma[i][j]);
+void matrizpLista(int m,Lista *lista, Graph ma){
+     for(int i = 0; i<ma->V;i++){
+      for(int j = 0; j<ma->V;j++){
+        inserir_fim(lista,ma->adj[i][j]);
       }
     }
   mostrar_lista(lista);
@@ -257,15 +336,11 @@ int menu(){
   
     int opc;
     system("clear");
-    printf("A matriz deve ser preenchida desta forma:");
-    printf("\n0 1 0 1 1\n1 0 1 1 1\n0 1 0 1 0\n1 1 1 0 1 \n1 1 0 1 0\n\n");
-    printf("A lista deve ser preenchida desta forma:");
-    printf("\n0\n1\n0\n1\n1\n1\n0\n1\n1\n1\n0\n1\n0\n1\n0\n1\n1\n1\n0\n1 \n1\n1\n0\n1\n0");
     printf("\n\n\n[0] - Sair.\n");
     printf("[1] - Matriz de adjacência para matriz de incidência.\n");
     printf("[2] - Matriz de incidência para matriz de adjacência.\n");
     printf("[3] - Matriz de adjacência para lista de adjacência e o inverso.\n");
-    printf("[4] - Lista de adjacência para matrizde adjacência.\n");
+    printf("[4] - Lista de adjacência para matriz de adjacência.\n");
     printf("Escolha uma opção: ");
     scanf("%i", &opc);
     return opc;
@@ -273,30 +348,32 @@ int menu(){
 
 int main(void) {
   int opc,n,m,num;
-  int ma[m][m];
+  Graph G;
   Lista *a = NULL;
   a=criar_lista();
    while (opc=menu()){
      system("clear");
         switch (opc){
         case 1:
-          printf("Digite a quantidade de vertices: ");
-          scanf("%i", &m);
-          printf("Digite a matriz sepada por espaco!\n\n");
-          Matrizadj_Inc(m);
+          scanf("%d %d",&n,&m);
+          G=GRAPHinit(n);
+          GRAPHleitura(G,m);
+          GRAPHshow(G);
+          Matrizadj_Inc(m,G);
+          //->adj[i][j]
             break;
         case 2:
-          printf("Digite a quantidade de vertices: ");
-          scanf("%i", &m);
-          printf("Digite a quantidade de aresta: ");
-          scanf("%i", &n);
-          printf("Digite a matriz sepada por espaco!\n\n");
+          printf("Digite a quantidade de aresta e vertices: ");
+          scanf("%d %d",&n,&m);
+          printf("Digite a matriz apenas com 0, 1 e 2 sepada por espaco!\n\n");
           MatrizInc_Adj(n,m);
             break;
         case 3:
-          printf("Digite a quantidade de vertices: ");
-          scanf("%i", &m);
-          matrizpLista(m,a);
+          scanf("%d %d",&n,&m);
+          G=GRAPHinit(n);
+          GRAPHleitura(G,m);
+          GRAPHshow(G);
+          matrizpLista(m,a,G);
             break;
         case 4:
           free(a);
@@ -304,7 +381,7 @@ int main(void) {
           printf("Digite a quantidade de vertices: ");
           scanf("%i", &m);
           system("clear");
-          printf("Digite a lista de forma encadeada:\n");
+          printf("Digite a lista encadeada [1 ou 0]:\n");
           for(int j = 0; j<pow(m,2);j++){
             scanf("%i", &num);
             inserir_fim(a,num);
